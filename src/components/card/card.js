11cd7343a0b0e0d0cc0cb1a4fac99fefed974929
Gnova09 <Georges.bueno@hotmail.com>
2022-06-20 12:46:56 -0400

    Changing AppCss
    Se agrego las etiquetas del css para los div del medio

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,16 +2,17 @@ import React from "react";
 
 const Card = (props) =>{
     const {name, carrera, photo} = props;
-    return(
-        <div className = 'card'>
-            <div className="img-content">
-                <img alt="placeholder" src={photo}/>
-                <p>{carrera}</p>
+    
+        return(
+            <div className = 'card '>
+                <div className="img-content">
+                    <img alt="placeholder" src={photo}/>
+                    <p>{carrera}</p>
+                </div>
+                <div>
+                    <h2>{name}</h2>
+                </div>
             </div>
-            <div>
-                <h2>{name}</h2>
-            </div>
-        </div>
-    )
+        )
 }
 export default Card;
